02046c81342e0544d7eae4bec9e75245d91a51eb
aleksey <a.m.koshmar@gmail.com>
2018-02-25 16:43:04 +0200

    changed .ignore file

--- lesson_02HW/src/main/java/com/koshmar/lesson_02HW/Main1.java
+++ lesson_02HW/src/main/java/com/koshmar/lesson_02HW/Main1.java
@@ -14,8 +14,6 @@ public class Main1 {
         //Инициализация ChromeDriver
         WebDriver driver = initChromeDriver();
 
-        Thread thread = new Thread();
-
         //Настройка ChromeDriver
         driver.get("http://prestashop-automation.qatestlab.com.ua/admin147ajyvk0/");
         driver.manage().window().maximize();
@@ -29,54 +27,121 @@ public class Main1 {
         WebElement button = driver.findElement(By.name("submitLogin"));
         button.click();
 
-        //
+        //Поиск и клик на элементе Dashboard
         WebElement dashBoard = driver.findElement(By.id("tab-AdminDashboard"));
         dashBoard.click();
+        //Вывод названия элемента в консоль
         WebElement dash = driver.findElement(By.linkText("Dashboard"));
-        System.out.println(dash.getText());
+        String stringDash = dash.getText();
+        System.out.println(stringDash);
+        //Перезагрузка страницы
         driver.navigate().refresh();
-        Thread.sleep(5000);
+        //Создание переменной для хранения названия раздела после перезагрузки
         WebElement dash1 = driver.findElement(By.linkText("Dashboard"));
-        if (dash.equals(dash1)){
+        String stringDash1 = dash1.getText();
+        //Сравнение и вывод результата в консоль
+        if (stringDash.equals(stringDash1)){
             System.out.println("Пользователь остается в разделе Dashboard");
         } else {
             System.out.println("Пользователь не остается в разделе Dashboard");
         }
 
+        //Поиск и клик на элементе orders
         WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
         orders.click();
+        //Вывод названия элемента в консоль
         WebElement ord = driver.findElement(By.linkText("Заказы"));
-        System.out.println(ord.getText());
+        String stringOrd = ord.getText();
+        System.out.println(stringOrd);
+        //Перезагрузка страницы
         driver.navigate().refresh();
-        Thread.sleep(5000);
+        //Создание переменной для хранения названия раздела после перезагрузки
         WebElement ord1 = driver.findElement(By.linkText("Заказы"));
-        if (ord.equals(ord1)){
+        String stringOrd1 = ord1.getText();
+        //Сравнение и вывод результата в консоль
+        if (stringOrd.equals(stringOrd1)){
             System.out.println("Пользователь остается в разделе Заказы");
         } else {
             System.out.println("Пользователь не остается в разделе Заказы");
         }
 
-
-
-
-
-
-//        WebElement orders = driver.findElement(By.id("subtab-AdminParentOrders"));
-//        orders.click();
-//        System.out.println("Заказы");
-
+//        //Поиск и клик на элементе catalog
 //        WebElement catalog = driver.findElement(By.id("subtab-AdminCatalog"));
 //        catalog.click();
-//
-//        WebElement parentCustomer = driver.findElement(By.id("subtab-AdminParentCustomer"));
-//        parentCustomer.click();
-//
-//        WebElement parentCustomerThreads = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
-//        parentCustomerThreads.click();
-//
-//        WebElement stats = driver.findElement(By.id("subtab-AdminStats"));
-//        stats.click();
-//
+//        //Вывод названия элемента в консоль
+//        WebElement cat = driver.findElement(By.linkText("Каталог"));
+//        String stringCat = cat.getText();
+//        System.out.println(stringCat);
+//        //Перезагрузка страницы
+//        driver.navigate().refresh();
+////        Thread.sleep(3000);
+//        //Создание переменной для хранения названия раздела после перезагрузки
+//        WebElement cat1 = driver.findElement(By.linkText("Каталог"));
+//        String stringCat1 = cat1.getText();
+//        //Сравнение и вывод результата в консоль
+//        if (stringCat.equals(stringCat1)){
+//            System.out.println("Пользователь остается в разделе Каталог");
+//        } else {
+//            System.out.println("Пользователь не остается в разделе Каталог");
+//        }
+
+        //Поиск и клик на элементе customer
+        WebElement parentCustomer = driver.findElement(By.id("subtab-AdminParentCustomer"));
+        parentCustomer.click();
+        //Вывод названия элемента в консоль
+        WebElement cust = driver.findElement(By.linkText("Клиенты"));
+        String stringCustomer = cust.getText();
+        System.out.println(stringCustomer);
+        //Перезагрузка страницы
+        driver.navigate().refresh();
+        //Создание переменной для хранения названия раздела после перезагрузки
+        WebElement cust1 = driver.findElement(By.linkText("Клиенты"));
+        String stringCustomer1 = cust1.getText();
+        //Сравнение и вывод результата в консоль
+        if (stringCustomer.equals(stringCustomer1)){
+            System.out.println("Пользователь остается в разделе Клиенты");
+        } else {
+            System.out.println("Пользователь не остается в разделе Клиенты");
+        }
+
+        //Поиск и клик на элементе customer service
+        WebElement parentCustomerThreads = driver.findElement(By.id("subtab-AdminParentCustomerThreads"));
+        parentCustomerThreads.click();
+        //Вывод названия элемента в консоль
+        WebElement custServ = driver.findElement(By.linkText("Служба поддержки"));
+        String stringCustomerService = custServ.getText();
+        System.out.println(stringCustomerService);
+        //Перезагрузка страницы
+        driver.navigate().refresh();
+        //Создание переменной для хранения названия раздела после перезагрузки
+        WebElement custServ1 = driver.findElement(By.linkText("Служба поддержки"));
+        String stringCustomerService1 = custServ1.getText();
+        //Сравнение и вывод результата в консоль
+        if (stringCustomerService.equals(stringCustomerService1)){
+            System.out.println("Пользователь остается в разделе Служба поддержки");
+        } else {
+            System.out.println("Пользователь не остается в разделе Служба поддержки");
+        }
+
+        //Поиск и клик на элементе customer
+        WebElement stats = driver.findElement(By.id("subtab-AdminStats"));
+        stats.click();
+        //Вывод названия элемента в консоль
+        WebElement stat = driver.findElement(By.linkText("Статистика"));
+        String stringStat = stat.getText();
+        System.out.println(stringStat);
+        //Перезагрузка страницы
+        driver.navigate().refresh();
+        //Создание переменной для хранения названия раздела после перезагрузки
+        WebElement stat1 = driver.findElement(By.linkText("Статистика"));
+        String stringStat1 = stat1.getText();
+        //Сравнение и вывод результата в консоль
+        if (stringStat.equals(stringStat1)){
+            System.out.println("Пользователь остается в разделе Статистика");
+        } else {
+            System.out.println("Пользователь не остается в разделе Статистика");
+        }
+
 //        WebElement parentModulesSf = driver.findElement(By.id("subtab-AdminParentModulesSf"));
 //        parentModulesSf.click();
 //
@@ -98,21 +163,7 @@ public class Main1 {
 //        WebElement advancedParameters = driver.findElement(By.id("subtab-AdminAdvancedParameters"));
 //        advancedParameters.click();
 
-
-
-
-
-
-//        WebElement ord = driver.findElement(By.partialLinkText("AdminDashboard&token=f57b9e0913a4757c6ba02314cf473896"));
-//        System.out.println(ord.getText());
-
-//        String ord = driver.findElement(By);
-
-//        List<WebElement> refers =driver.findElements(By.tagName("b_algo"));
-//        System.out.println(refers.size());
-
-
-//        driver.quit();
+        driver.quit();
 
     }
 
@@ -121,7 +172,4 @@ public class Main1 {
         return new ChromeDriver();
     }
 
-//    private static Thread initThread (){
-//
-//    }
 }
